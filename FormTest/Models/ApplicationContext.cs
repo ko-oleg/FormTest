a27aafc2948434078a3bcc94787755f50189ba72
Oleg Kogay <olegatorkogay@gmail.com>
2023-01-19 16:01:12 +0600

    Create Migration

---
+++ FormTest/Models/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FormTest.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Form> Forms { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+    }
+}
--- FormTest/Models/Form.cs
+++ FormTest/Models/Form.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace FormTest.Models
 {
+    [Keyless]
     public class Form
     {
         [Required]
