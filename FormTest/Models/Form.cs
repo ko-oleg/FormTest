997f15dd67dfefc7bcf4d98364cbf264209e5157
Oleg Kogay <olegatorkogay@gmail.com>
2023-01-19 15:49:54 +0600

    Create Model

---
+++ FormTest/Models/Form.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormTest.Models
+{
+    public class Form
+    {
+        [Required]
+        public string Fullname { get; set; }
+        [Phone]
+        public string MobilePhone { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string CompanyName { get; set; }
+        public string Post { get; set; }
+    }
+}
