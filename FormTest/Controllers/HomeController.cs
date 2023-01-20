b4fe0136a54cbe93e471123bcb06ceb91e89bc25
Oleg Kogay <olegatorkogay@gmail.com>
2023-01-20 15:26:02 +0600

    Create delete and edit methods

--- FormTest/Controllers/HomeController.cs
+++ FormTest/Controllers/HomeController.cs
@@ -52,11 +52,58 @@ namespace FormTest.Controllers
             return NotFound();
         }
         
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id!=null)
+            {
+                Form form = await db.Forms.FirstOrDefaultAsync(p=>p.Id==id);
+                if (form != null)
+                    return View(form);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Form form)
+        {
+            db.Forms.Update(form);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        
         public async Task<IActionResult> SendMessage(Form form)
         {
             EmailService emailService = new EmailService();
             await emailService.SendEmailAsync(form.Email, "Создание формы", $"Ваша форма успешно создалась!<p>ФИО: {form.Fullname} <p>Телефон: {form.MobilePhone}<p>Email: {form.Email}<p>Название компании: {form.CompanyName}<p>Должность: {form.Post}");
             return RedirectToAction("Index");
+        }
+        
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                Form form = await db.Forms.FirstOrDefaultAsync(p => p.Id == id);
+                if (form != null)
+                    return View(form);
+            }
+            return NotFound();
+        }
+ 
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                Form form = await db.Forms.FirstOrDefaultAsync(p => p.Id == id);
+                if (form != null)
+                {
+                    db.Forms.Remove(form);
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            return NotFound();
         }
 
         public IActionResult Privacy()
